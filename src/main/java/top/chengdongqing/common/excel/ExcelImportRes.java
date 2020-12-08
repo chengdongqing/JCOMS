9bdc30eb1e995c0b5f5b251b6034098ff444d7a5
Luyao <1912525497@qq.com>
2020-12-08 10:04:23 +0800

    优化已知问题

--- src/main/java/top/chengdongqing/common/excel/ExcelImportRes.java
+++ src/main/java/top/chengdongqing/common/excel/ExcelImportRes.java
@@ -1,9 +1,6 @@
 package top.chengdongqing.common.excel;
 
-import lombok.Getter;
-import lombok.Setter;
-
-import java.util.List;
+import java.util.Collection;
 
 /**
  * excel数据导入响应实体
@@ -12,16 +9,13 @@ import java.util.List;
  * @param <F> 失败的数据实体类型
  * @author Luyao
  */
-@Getter
-@Setter
-public class ExcelImportRes<S, F> {
-
-    /**
-     * 导入成功的数据信息
-     */
-    private List<S> successfulRows;
-    /**
-     * 导入失败的数据信息
-     */
-    private List<F> failedRows;
+public record ExcelImportRes<S, F>(
+        /**
+         * 导入成功的数据信息
+         */
+        Collection<S> successfulRows,
+        /**
+         * 导入失败的数据信息
+         */
+        Collection<F> failedRows) {
 }
